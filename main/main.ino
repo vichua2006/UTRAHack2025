5f2a905dce488f58e6ca27c243b6c0619acffc5b
honzikschenk <nojschenk@gmail.com>
2025-02-01 15:07:36 -0500

    Add color pins 0 and 1, and input tuned min and max

--- main/main.ino
+++ main/main.ino
@@ -1,44 +1,49 @@
+#define COLOR_S0 5
+#define COLOR_S1 6
 #define COLOR_S2 2
 #define COLOR_S3 3
 
 #define COLOR_PWM 4
 
-// #define RED_MIN 
-// #define RED_MAX 
+// #define RED_MIN 15
+// #define RED_MAX 110
 // #define RED_THRESHOLD 
 
-// #define GREEN_MIN 
-// #define GREEN_MAX 
+// #define GREEN_MIN 20
+// #define GREEN_MAX 130
 // #define GREEN_THRESHHOLD 
 
-// #define BLUE_MIN 
-// #define BLUE_MAX 
+// #define BLUE_MIN 20
+// #define BLUE_MAX 130
 // #define BLUE_THRESHHOLD 
 
 void setup()
 {
     Serial.begin(115200);
 
+    pinMode(COLOR_S0, OUTPUT);
+    pinMode(COLOR_S1, OUTPUT);
     pinMode(COLOR_S2, OUTPUT);
     pinMode(COLOR_S3, OUTPUT);
 
     pinMode(COLOR_PWM, INPUT);
+
+    digitalWrite(COLOR_S0, HIGH);
+	  digitalWrite(COLOR_S1, HIGH);
 }
 
 void loop()
 {
-  // Serial.print("Red: ");
-  // Serial.print(readRed());
-  // Serial.print("\n");
-  // delay(100);
+  Serial.print("Red: ");
+  Serial.print(readRed());
+  Serial.print(" - ");
   Serial.print("Green: ");
   Serial.print(readGreen());
+  Serial.print(" - ");
+  Serial.print("Blue: ");
+  Serial.print(readBlue());
   Serial.print("\n");
-  delay(100);
-  // Serial.print("Blue: ");
-  // Serial.print(readBlue());
-  // Serial.print("\n");
-  // delay(100);
+  delay(200);
 }
 
 int readRed() {
@@ -47,9 +52,9 @@ int readRed() {
 
   int pw = pulseIn(COLOR_PWM, LOW);
 
-  return pw;
+  // pw = map(pw, RED_MIN, RED_MAX, 255, 0);
 
-  // return map(pw, RED_MIN, RED_MAX, 255, 0);
+  return pw;
 }
 
 int readGreen() {
@@ -58,9 +63,9 @@ int readGreen() {
 
   int pw = pulseIn(COLOR_PWM, LOW);
 
-  return pw;
+  // pw = map(pw, GREEN_MIN, GREEN_MAX, 255, 0);
 
-  // return map(pw, GREEN_MIN, GREEN_MAX, 255, 0);
+  return pw;
 }
 
 int readBlue() {
@@ -69,7 +74,7 @@ int readBlue() {
 
   int pw = pulseIn(COLOR_PWM, LOW);
 
-  return pw;
+  // pw = map(pw, BLUE_MIN, BLUE_MAX, 255, 0);
 
-  // return map(pw, BLUE_MIN, BLUE_MAX, 255, 0);
+  return pw;
 }
