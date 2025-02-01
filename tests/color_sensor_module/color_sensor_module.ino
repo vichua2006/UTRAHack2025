62efb2f3e67483b181104c5758ca9d0c19caad29
Rohan Katreddy <rohan.katreddy@gmail.com>
2025-02-01 17:09:29 -0500

    implemented and tested color detection module

---
+++ tests/color_sensor_module/color_sensor_module.ino
@@ -0,0 +1,88 @@
+#define  s0 8       //Module pins wiring
+#define s1 9
+#define s2 10
+#define s3 11
+#define  out 12
+#define SAMPLE_COUNT 10
+#define CONFIDENCE_THRESHOLD 35
+
+enum color{
+  red, green, blue, error
+};
+
+void  setup() 
+{
+   pinMode(s0,OUTPUT);    //pin modes
+   pinMode(s1,OUTPUT);
+   pinMode(s2,OUTPUT);
+   pinMode(s3,OUTPUT);
+   pinMode(out,INPUT);
+
+   Serial.begin(9600);   //intialize the serial monitor baud rate
+   
+   digitalWrite(s0,HIGH);  //Putting S0/S1 on HIGH/HIGH levels means the output frequency scalling is at 100%  (recommended)
+   digitalWrite(s1,LOW); //LOW/LOW is off HIGH/LOW is 20% and  LOW/HIGH is  2%
+   
+}
+
+void loop()                  //Every 2s we select  a photodiodes set and read its data
+{
+  Serial.println(getEnumColor(getColor()));
+}
+
+
+int getData(int samples){
+  int data = -1;
+  for (int i = 0; i < samples; i++){
+   //delay(5);
+   data+=pulseIn(out,HIGH);       //here  we wait until "out" go LOW, we start measuring the duration and stops when "out"  is HIGH again
+  };
+  return data/samples;
+}
+
+enum color getColor(){
+   digitalWrite(s2,LOW);        //S2/S3  levels define which set of photodiodes we are using LOW/LOW is for RED LOW/HIGH  is for Blue and HIGH/HIGH is for green
+   digitalWrite(s3,LOW);
+   int redVal = getData(SAMPLE_COUNT);                   //Executing GetData function to  get the value
+
+   digitalWrite(s2,LOW);
+   digitalWrite(s3,HIGH);
+   int blueVal = getData(SAMPLE_COUNT);
+
+   digitalWrite(s2,HIGH);
+   digitalWrite(s3,HIGH);
+   int greenVal = getData(SAMPLE_COUNT);
+
+   if (redVal < greenVal && redVal < blueVal){
+    int confidence = min(abs(greenVal - redVal), abs(blueVal - redVal));
+    if (confidence >= CONFIDENCE_THRESHOLD) return red;
+    else return error;
+   }
+   else if (greenVal < redVal && greenVal < blueVal){
+    int confidence = min(abs(redVal - greenVal), abs(blueVal - greenVal));
+    if (confidence >= CONFIDENCE_THRESHOLD) return green;
+    else return error;
+    }
+   else if (blueVal < greenVal && blueVal < redVal){
+    int confidence = min(abs(redVal - blueVal), abs(greenVal - blueVal));
+    if (confidence >= CONFIDENCE_THRESHOLD) return blue;
+    else return error;
+    }
+   else return error;
+}
+
+String getEnumColor(enum color c) 
+{
+   switch (c) 
+   {
+      case red: return "red";
+      case blue: return "blue";
+      case green: return "green";
+      case error: return "error";
+   }
+}
+
+bool isColorFound(){
+  enum color result = getColor();
+  return (result == red || result == green || result == blue);
+}
