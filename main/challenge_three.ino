c49d3b5e7953fa8bc93bdb00fb4f74bb82a97c82
FATCullen <98052707+FATCullen@users.noreply.github.com>
2025-02-02 05:16:59 -0500

    Boilerplate code for 3 - Datastructure

---
+++ main/challenge_three.ino
@@ -0,0 +1,60 @@
+enum direction{
+  up,
+  down,
+  left,
+  right
+};
+
+char path[14][14];
+direction previous = down;
+
+bool detectWall(){
+  float repetitions = 5.0;
+  int threshhold = 15;
+  float average = 0;
+
+  for(int i = 0; i < repetitions; i++) average += getDistance() / repetitions;
+  if(average < threshhold) return true;
+
+  return false;
+}
+
+// vvv FOR SOME REASON THIS IS NECESSARY vvv
+char getCurrentNode(enum direction last);
+char getCurrentNode(enum direction last){
+  char ans = 0;
+  switch(getColor()){
+    case red: 
+      ans += 192;
+      break;
+    case green: 
+      ans += 128;
+      break;
+    case blue: 
+      ans += 64;
+      break;
+  }
+
+  switch(last){
+    case up:
+      ans += 48;
+      break;
+    case right:
+      ans += 32;
+      break;
+    case down:
+      ans += 16;
+      break;
+  }
+
+  for(int i = 1; i <= 8; i << 1){
+    if(detectWall) ans += i;
+    //turn90() **********
+  }
+
+  return ans;
+}
+
+void challengeThree(){
+  return;
+}
