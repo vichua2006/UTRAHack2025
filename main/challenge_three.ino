3ef4d440da2aea57f6c1bb1744b8eaec4e19832b
FATCullen <98052707+FATCullen@users.noreply.github.com>
2025-02-02 13:34:55 -0500

    Much better (less bad) C3 algorithm

--- main/challenge_three.ino
+++ main/challenge_three.ino
@@ -1,80 +1,46 @@
+const float MIN_WALL_WIDTH = 100.0;
+const float COURSE_SIZE = 1500.0;
 
-// enum direction{
-//   up,
-//   down,
-//   left,
-//   right
-// };
-
-
-// char path[30][30];
-// direction previous = down;
-
-// bool detectWall(){
-//   float repetitions = 5.0;
-//   int threshhold = 15;
-//   float average = 0;
-
-//   for(int i = 0; i < repetitions; i++) average += getDistance() / repetitions;
-//   if(average < threshhold) return true;
-
-//   return false;
-// }
-
-// // vvv FOR SOME REASON THIS IS NECESSARY vvv
-// char getCurrentNode(enum direction last);
-// char getCurrentNode(enum direction last){
-//   char ans = 0;
-//   switch(getColor()){
-//     case red: 
-//       ans += 192;
-//       break;
-//     case green: 
-//       ans += 128;
-//       break;
-//     case blue: 
-//       ans += 64;
-//       break;
-//   }
-
-//   switch(last){
-//     case up:
-//       ans += 48;
-//       break;
-//     case right:
-//       ans += 32;
-//       break;
-//     case down:
-//       ans += 16;
-//       break;
-//   }
-
-//   for(int i = 1; i <= 8; i << 1){
-//     if(detectWall) ans += i;
-//     //turn90() **********
-//   }
-
-//   return ans;
-// }
-
-// void challengeThree(){
-//   return;
-// }
-
-void challengeThree(color path[], int size){
-  while(getDistance() < MIN_SONIC_DISTANCE) turnLeft();
-
-  for(int i = 0; i < size; i++){
-    while(getColor() != path[i]){
-      driveMotor(DRIVE_SPEED, DRIVE_SPEED);
-      if(getDistance() < MIN_SONIC_DISTANCE){
-        turnLeft(); turnLeft();
-      }
+void challengeThree(color path[], int size) {
+  int pathIndex = 0;
+  
+  while(pathIndex < size) {
+    color currentColor = getColor();
+    if(currentColor == path[pathIndex]) {
+      driveMotor(0, 0);
+      delay(200);
+      ledBlink();
+      delay(200);
+      pathIndex++;
     }
-
-    driveMotor(0,0);
-    delay(500);
-    ledBlink();
-    delay(500);
+    
+    float frontDist = getDistance();
+    
+    if(frontDist < MIN_SONIC_DISTANCE) {
+      driveMotor(0, 0);
+      
+      turnRight();
+      float rightDist = getDistance();
+      
+      if(rightDist > MIN_SONIC_DISTANCE + MIN_WALL_WIDTH) {
+        driveMotor(DRIVE_SPEED, DRIVE_SPEED);
+        continue;
+      }
+      
+      turnLeft();
+      turnLeft();
+      float leftDist = getDistance();
+      
+      if(leftDist > MIN_SONIC_DISTANCE + MIN_WALL_WIDTH) {
+        driveMotor(DRIVE_SPEED, DRIVE_SPEED);
+        continue;
+      }
+      turnLeft();
+      
+    } else {
+      driveMotor(DRIVE_SPEED, DRIVE_SPEED);
+    }
   }
+  
+  driveMotor(0, 0);
 }
